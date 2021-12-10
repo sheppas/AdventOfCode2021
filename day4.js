f4d3200158d0176999744483695519d3daee89b4
Andrew Favaloro <andrew.favaloro@gmail.com>
2021-12-09 21:21:39 -0500

    Add some tests
    Something weird is happening with these tests, eh?
    See what you can make of it.

--- day4.js
+++ day4.js
@@ -84,3 +84,29 @@ const bingoSquid = (calledArray, boardArray) => {
 bingoSquid(exampleCalled, exampleBoards)
 
 
+// TESTS
+// TEST BOARD HELPER
+console.log(boardHelper([1,2,3,4,5], [1,2,3,4,5]) === true)
+console.log(boardHelper([1,2,3,4,5], [0,2,3,4,5]) === false)
+console.log(boardHelper([1,2,3,4,5], [0,1,2,3,4,5,6,7]) == true)
+
+// TEST FIND LAST BOARD
+const boardA = [
+  [ 1, 2, 3, 4, 5],
+  [ 6, 7, 8, 9,10],
+  [11,12,13,14,15],
+  [16,17,18,19,20],
+  [21,22,23,24,25]
+]
+const boardB = [
+  [10,20,30,40,50],
+  [ 6, 7, 8, 9,10],
+  [11,12,13,14,15],
+  [16,17,18,19,20],
+  [21,22,23,24,25]
+]
+
+console.log(bingoSquid([1,2,3,4,5],[boardA,boardB]) == boardA);
+console.log(bingoSquid([10,20,30,40,50],[boardA,boardB]) == boardB);
+console.log(bingoSquid([1,2,3,4,5,10,20,30,40,50], [boardA,boardB]) == boardB)
+console.log(bingoSquid([1,2,3,4,5,10,20,30,40,50], [boardB,boardA]) == boardB)
